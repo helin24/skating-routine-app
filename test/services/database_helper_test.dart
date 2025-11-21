3ccb24844fe34cdae9934ad2d6c5a3446d01d2bb
Helin Shiah <helinx@google.com>
2025-11-21 11:46:43 -0800

    Remove test for initial elements

--- test/services/database_helper_test.dart
+++ test/services/database_helper_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:skating_routine_app/src/services/database_helper.dart';
-import 'package:skating_routine_app/src/services/initial_data.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
@@ -26,12 +25,6 @@ void main() {
     test('database can be initialized', () async {
       final db = await dbHelper.database;
       expect(db.isOpen, isTrue);
-    });
-
-    test('initial elements are populated on creation', () async {
-      final elements = await dbHelper.getElements();
-      expect(elements.length, initialElements.length);
-      expect(elements.first.name, initialElements.first.name);
     });
   });
 }
